f05637085fa5da92221303f5b2019f76157af292
trinhthehung <30575263+trinhthehung@users.noreply.github.com>
2019-03-07 13:37:19 +0700

    hieu ung day tam giac ra

--- js.js
+++ js.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded",function(){
     btn_switch=Array.prototype.slice.call(document.querySelectorAll(".btn_switch>ul>li"));
+    slides =Array.prototype.slice.call(document.querySelectorAll(".parent>.slide"));
     btn_switch.forEach(element => {
         element.addEventListener("click",function(){
             this.classList.add("btn_switch_clicked");
             btn_switch.forEach(e => {
             	if(e!=this)
             	{
-            		e.classList.remove("btn_switch_clicked");
-            	}
+                    e.classList.remove("btn_switch_clicked");
+                    var slide= document.getElementById(e.getAttribute("data-slide"));
+                    // e.classList.remove("slide_enable");
+                    slide.classList.remove("slide_enable");
+                }
+                    var slide= document.getElementById(this.getAttribute("data-slide"));
+                    slide.classList.add("slide_enable");
             })
         })
     });
