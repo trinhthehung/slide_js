5671ecbb0662e572f4d2a20f64b43dfc97e03442
trinhthehung <30575263+trinhthehung@users.noreply.github.com>
2019-03-14 13:52:37 +0700

    finish

--- js.js
+++ js.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded",function(){
     btn_switch.forEach(element => {
         
         element.addEventListener("click",function(){
-           
-           
             this.classList.add("btn_switch_clicked");
             btn_switch.forEach(e => {
             	if(e!=this)
@@ -15,20 +13,9 @@ document.addEventListener("DOMContentLoaded",function(){
                     e.classList.remove("btn_switch_clicked");
                     var slide= document.getElementById(e.getAttribute("data-slide"));
                     slide.classList.remove("slide_enable");
-                    var querySelectorImg = `#${e.getAttribute("data-slide")}>img`;
-                    var imgs= document.querySelectorAll(querySelectorImg);
-                    imgs[0].classList.remove("amt_tamgiac_left1");
-                    imgs[1].classList.remove("amt_tamgiac_left2");
-                    imgs[2].classList.remove("amt_tamgiac_right");
-                    
                 }
                     var slide= document.getElementById(this.getAttribute("data-slide"));
                     slide.classList.add("slide_enable");
-                    var querySelectorImg = `#${this.getAttribute("data-slide")}>img`;
-                    var imgs= document.querySelectorAll(querySelectorImg);
-                    imgs[0].classList.add("amt_tamgiac_left1");
-                    imgs[1].classList.add("amt_tamgiac_left2");
-                    imgs[2].classList.add("amt_tamgiac_right");
                     clearInterval(_autoSwitchSlide);
                     resetAutoSwitchSlide();
             })
@@ -36,58 +23,49 @@ document.addEventListener("DOMContentLoaded",function(){
     });
     //auto switch slide
     function autoSwitchSlide(){
-        removeAllAnimationClass();
         for(var i=0;i<btn_switch.length;i++)
         {
-
-            for(var j=0;j<btn_switch.length;j++){
-                var slide= document.getElementById(btn_switch[j].getAttribute("data-slide"));
-                slide.classList.remove("slide_enable");
+            if(btn_switch[i].classList[0]==="btn_switch_clicked")
+            {
+                var slide= document.getElementById(btn_switch[i].getAttribute("data-slide"));
+                slide.querySelectorAll("img")[0].classList.add("tamgiac_left1_hidden");
+                slide.querySelectorAll("img")[1].classList.add("tamgiac_left2_hidden");
+                slide.querySelectorAll("img")[2].classList.add("tamgiac_right_hidden");
             }
+            else{
+                var slide= document.getElementById(btn_switch[i].getAttribute("data-slide"));
+                slide.querySelectorAll("img")[0].classList.remove("tamgiac_left1_hidden");
+                slide.querySelectorAll("img")[1].classList.remove("tamgiac_left2_hidden");
+                slide.querySelectorAll("img")[2].classList.remove("tamgiac_right_hidden");
+            }
+        }
+        setTimeout(() => {
+             for(var i=0;i<btn_switch.length;i++)
+        {
+            var slide= document.getElementById(btn_switch[i].getAttribute("data-slide"));
+            slide.classList.remove("slide_enable");
             if(btn_switch[i].classList[0]==="btn_switch_clicked"){
                 btn_switch[i].classList.remove("btn_switch_clicked");
                 if(i===2)
                 {
-
-                        btn_switch[0].classList.add("btn_switch_clicked");
-                        var slide= document.getElementById(btn_switch[0].getAttribute("data-slide"));
-                        slide.classList.add("slide_enable");
-
-                        var querySelectorImg = `#${btn_switch[0].getAttribute("data-slide")}>img`;
-                        var imgs= document.querySelectorAll(querySelectorImg);
-                        imgs[0].classList.add("amt_tamgiac_left1");
-                        imgs[1].classList.add("amt_tamgiac_left2");
-                        imgs[2].classList.add("amt_tamgiac_right");
+                            btn_switch[0].classList.add("btn_switch_clicked");
+                            var slide= document.getElementById(btn_switch[0].getAttribute("data-slide"));
+                            slide.classList.add("slide_enable");  
                 }
                 else{
+                   
                         btn_switch[i+1].classList.add("btn_switch_clicked");
                         var slide= document.getElementById(btn_switch[i+1].getAttribute("data-slide"));
                         slide.classList.add("slide_enable");
-
-                        var querySelectorImg = `#${btn_switch[i+1].getAttribute("data-slide")}>img`;
-                        var imgs= document.querySelectorAll(querySelectorImg);
-                        imgs[0].classList.add("amt_tamgiac_left1");
-                        imgs[1].classList.add("amt_tamgiac_left2");
-                        imgs[2].classList.add("amt_tamgiac_right");
                 }
                 break;
             }
-        }
+        }       
+        },1000);
     }
-    // var autoSwitchSlide=setInterval(autoSwitchSlide,3000);
     function resetAutoSwitchSlide()
     {
-        _autoSwitchSlide=setInterval(autoSwitchSlide,3000);
+        _autoSwitchSlide=setInterval(autoSwitchSlide,5000);
     }
     resetAutoSwitchSlide();
-    //function remove all class animation
-    function removeAllAnimationClass(){
-        btn_switch.forEach((e)=>{
-                var querySelectorImg = `#${e.getAttribute("data-slide")}>img`;
-                var imgs= document.querySelectorAll(querySelectorImg);
-                imgs[0].classList.remove("amt_tamgiac_left1");
-                imgs[1].classList.remove("amt_tamgiac_left2");
-                imgs[2].classList.remove("amt_tamgiac_right");
-        })
-    }
 },false)
